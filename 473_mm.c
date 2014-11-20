34ebc2b242f1b23915e3f17cd94f6c3f41e5560c
Jackson Burlew <jackburlew5@gmail.com>
2014-11-20 18:45:08 -0500

    First commit.

---
+++ 473_mm.c
@@ -0,0 +1,11 @@
+void mm_init(void* vm, int vm_size, int n_frames, int page_size, int policy) {
+
+}
+
+unsigned long mm_report_npage_faults() {
+	return 0;
+}
+
+unsigned long mm_report_nwrite_backs() {
+	return 0;
+}
