42ab168e929ffdb5b9190d163bd68f7e44911b1e
Jackson Burlew <jackburlew5@gmail.com>
2014-11-21 18:18:37 -0500

    Finished clock replacement.

--- 473_mm.c
+++ 473_mm.c
@@ -3,10 +3,11 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 #include <stdio.h>
 #include <stdarg.h>
-#define DEBUG_LEVEL 0b11111111
+#define DEBUG_LEVEL 0b11011000
 
 static void debug(int level, const char *fmt, ...) {
 	if (!(level & DEBUG_LEVEL))
@@ -36,8 +37,11 @@ struct rmem {
 // pool of resident memory.
 static struct rmem *pool;
 static int num_frames;
+static void* mem_start;
+static int PAGE_SIZE;
+static int page_faults = 0, write_backs = 0;
 
-static void clock_handler(int v, siginfo_t *si);
+static void clock_handler(int, siginfo_t*, void*);
 
 void mm_init(void* vm, int vm_size, int n_frames, int page_size, int policy) {
 	debug(1, "mm_init called: vm %p, vm_size: 0x%x, n_frames: %d, page_size: 0x%x, policy: %d\n", vm, vm_size, n_frames, page_size, policy);
@@ -51,33 +55,102 @@ void mm_init(void* vm, int vm_size, int n_frames, int page_size, int policy) {
 		pool = malloc(sizeof(struct rmem) * n_frames);
 		{
 			int i;
-			for (i = 0; i < n_frames - 1; i++) {
+			for (i = 0; i < n_frames; i++) {
 				pool[i].frame = -1;
 				pool[i].accessed = 0;
 				pool[i].dirty = 0;
-				pool[i].next = &pool[i+1 % n_frames];
+				pool[i].next = &pool[(i+1) % n_frames];
+			}
+			for (i = 0; i < n_frames; i++) {
+				debug(2, "pool[%d] addr: %p; next: %p\n", i, &pool[i], pool[i].next);
 			}
 		}
 
 		struct sigaction action;
-		action.sa_handler = clock_handler;
-		action.sa_flags = 0;
+		action.sa_sigaction = clock_handler;
+		action.sa_flags = SA_SIGINFO;
 		sigaction(SIGSEGV, &action, NULL);
 
-		//mprotect(vm, vm_size, PROT_NONE);
+		mprotect(vm, vm_size, PROT_NONE);
+		mem_start = vm;
+		PAGE_SIZE = getpagesize();
 
 		break;
 	}
 }
 
-static void clock_handler(int v, siginfo_t *si) {
-	debug(2, "SIGSEGV received: %p\n", si);
+static void dump() {
+	struct rmem *trace = pool;
+	do {
+		debug(32, "%p; frame: %d, accessed: %d, dirty: %d, next: %p\n", trace, trace->frame, trace->accessed, trace->dirty, trace->next);
+		trace = trace->next;
+	} while (trace != pool);
+}
+
+static void clock_handler(int v, siginfo_t *si, void *context) {
+	int page = (int) (si->si_addr - mem_start) / PAGE_SIZE;
+	debug(2, "SIGSEGV received: addr: %p\n", si->si_addr);
+	struct rmem *trace = pool;
+	int i;
+	dump();
+
+	debug(4, "page: %d", page);
+	// Check to see if the memory is already resident.
+	// If so, we should set its accessed bit or dirty bit,
+	// depending on the style of modification.
+	do {
+		if (trace->frame == page) {
+			debug(4, "; in-memory: %p", trace);
+			if (trace->accessed) {
+				debug(4, "; writing\n");
+				trace->dirty = 1;
+				mprotect(page*PAGE_SIZE + mem_start, PAGE_SIZE, PROT_READ | PROT_WRITE);
+			} else {
+				debug(4, "; reading\n");
+				trace->accessed = 1;
+				if (trace->dirty) {
+					mprotect(page*PAGE_SIZE + mem_start, PAGE_SIZE, PROT_READ | PROT_WRITE);
+				} else {
+					mprotect(page*PAGE_SIZE + mem_start, PAGE_SIZE, PROT_READ);
+				}
+			}
+			dump();
+			debug(2, "Exiting signal handler.\n");
+			return;
+		}
+		trace = trace->next;
+	} while (trace != pool);
+
+	page_faults++;
+	debug(4, "; not in memory [%d]", page_faults);
+
+	// If the memory is not resident, we should evict the next bad element.
+	while (pool->accessed) {
+		pool->accessed = 0;
+		// still dirty, but we should prevent reading so that we know if it gets accessed again.
+		mprotect(pool->frame*PAGE_SIZE + mem_start, PAGE_SIZE, PROT_NONE);
+		pool = pool->next;
+	}
+	debug(4, "; evicting: %d", pool->frame);
+	if (pool->dirty) {
+		debug(4, "; dirty");
+		write_backs++;
+	}
+	debug(4, "; emplace: %p\n", pool);
+	pool->frame = page;
+	pool->accessed = 1;
+	mprotect(page*PAGE_SIZE + mem_start, PAGE_SIZE, PROT_READ);
+	pool->dirty = 0;
+
+	pool = pool->next;
+	dump();
+	debug(2, "Exiting signal handler.\n");
 }
 
 unsigned long mm_report_npage_faults() {
-	return 0;
+	return page_faults;
 }
 
 unsigned long mm_report_nwrite_backs() {
-	return 0;
+	return write_backs;
 }
--- makefile
+++ makefile
@@ -1,18 +1,18 @@
 CFLAGS=-g
 LINKFLAGS=
 OBJFILES=473_mm.o test-code1.o test-code2.o test-code3.o test-code4.o test-code5.o test-code6.o
-OUTPUT_FILES=results.txt
+OUTPUT_FILES=results.txt test1.txt test2.txt test3.txt test4.txt test5.txt test6.txt
 EXECUTABLES=test-code1.exe test-code2.exe test-code3.exe test-code4.exe test-code5.exe test-code6.exe
 
 all: $(EXECUTABLES)
 
 test: $(EXECUTABLES)
-	./test-code1.exe >/dev/null; diff results.txt output_1.txt || echo '\033[1;31mFailed on test 1 \033[00m'
-	./test-code2.exe >/dev/null; diff results.txt output_2.txt || echo '\033[1;31mFailed on test 2 \033[00m'
-	./test-code3.exe >/dev/null; diff results.txt output_3.txt || echo '\033[1;31mFailed on test 3 \033[00m'
-	./test-code4.exe >/dev/null; diff results.txt output_4.txt || echo '\033[1;31mFailed on test 4 \033[00m'
-	./test-code5.exe >/dev/null; diff results.txt output_5.txt || echo '\033[1;31mFailed on test 5 \033[00m'
-	./test-code6.exe >/dev/null; diff results.txt output_6.txt || echo '\033[1;31mFailed on test 6 \033[00m'
+	./test-code1.exe >./test1.txt; diff test1.txt output_1.txt || echo '\033[1;31mFailed on test 1 \033[00m'
+	./test-code2.exe >./test2.txt; diff test2.txt output_2.txt || echo '\033[1;31mFailed on test 2 \033[00m'
+	./test-code3.exe >./test3.txt; diff test3.txt output_3.txt || echo '\033[1;31mFailed on test 3 \033[00m'
+	./test-code4.exe >./test4.txt; diff test4.txt output_4.txt || echo '\033[1;31mFailed on test 4 \033[00m'
+	./test-code5.exe >./test5.txt; diff test5.txt output_5.txt || echo '\033[1;31mFailed on test 5 \033[00m'
+	./test-code6.exe >./test6.txt; diff test6.txt output_6.txt || echo '\033[1;31mFailed on test 6 \033[00m'
 
 %.exe: %.o 473_mm.o 473_mm.h
 	gcc -o $@ $< 473_mm.o
